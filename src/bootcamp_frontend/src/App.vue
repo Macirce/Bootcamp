7e971f3c1b7fe7cfc2bff2558884a18f070082d6
Macirce <szymon.kalinowski@sluzbowy@gmail.com>
2024-06-24 22:25:18 +0200

    first commit

---
+++ src/bootcamp_frontend/src/App.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <main>
+    <img src="/logo2.svg" alt="DFINITY logo" />
+    Siemano
+    <br/>
+    <button @click="podbij">kliknij, aby zwiększyć liczbę</button> 
+    {{ licznik }}
+  </main>
+</template>
+
+<script>
+export  default {
+  data() {
+    return{
+      licznik:0
+    }
+  },
+  methods: {
+    podbij(){
+      this.licznik++
+    }
+  }
+}
+</script>
